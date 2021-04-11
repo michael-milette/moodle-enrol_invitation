7f93a73c733a19cafde135690ea2cd411e1eb0bc
Lukas Celinak <lukascelinak@gmail.com>
2021-04-11 14:59:24 +0200

    Milestone 4 Hotfix cohort invite

--- invitation.php
+++ invitation.php
@@ -108,7 +108,6 @@ if ($data and confirm_sesskey()) {
     $email_list = invitation_form::parse_dsv_emails($data->email, $delimiters);
     $userlistmails=invitation_form::parse_userlist_emails($data->userlist);
     $cohortmails=invitation_form::parse_cohortlist_emails($data->cohortlist,$course);
-    print_object($cohortmails);
     $email_list=array_merge($email_list, $userlistmails,$cohortmails);  
     $email_list = array_unique($email_list);
 
